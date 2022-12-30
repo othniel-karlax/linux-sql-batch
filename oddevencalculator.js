c35a9be6616368e0f22a9077e487965003549037
aaronsolomon65 <aaronsolomon65@gmail.com>
2022-12-30 03:09:57 -0800

    Aaron's code

---
+++ oddevencalculator.js
@@ -0,0 +1,13 @@
+function AarOn (){
+
+    var a=document.getElementById("qwe").value
+    var aaron=document.getElementById("zxc")
+
+    if (a%2==0){
+        z="this is even number"
+    }
+    else {
+        z="this is odd number"
+    }
+    aaron.innerText=z
+}
